09f6422673cfd9697a87381885b465673bb5bd07
Yoonje Choi <yoonje.choi@spotu.io>
2022-11-01 19:06:55 +0900

    query todo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { Amplify } from "aws-amplify";
+import awsconfig from "./aws-exports";
+
+import { DataStore } from '@aws-amplify/datastore';
+import { Todo } from './models';
+
+
+async function App() {
+  Amplify.configure(awsconfig);
+
+  const models = await DataStore.query(Todo);
+  const model = models[0];
+  console.log(model.name, model.description);
+
+  // await DataStore.save(
+  //   new Todo({
+  //     "name": "Lorem ipsum dolor sit amet",
+  //     "description": "Lorem ipsum dolor sit amet"
+  //   })
+  // );
+
   return (
     <div className="App">
       <header className="App-header">
